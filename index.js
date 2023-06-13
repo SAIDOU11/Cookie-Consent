17268a514a11c5064ffb9e49c8f71b58927604b4
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-06-13 17:33:08 +0200

    disabled element

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 const modal = document.getElementById("modal");
-const closeModal = document.getElementById("modal-close-btn");
-console.log(closeModal);
+const modalCloseBtn = document.getElementById("modal-close-btn");
+const consentForm = document.getElementById("consent-form");
+const modalText = document.getElementById("modal-text");
+const modalInner = document.getElementById("modal-inner");
 
-setTimeout(() => {
+setTimeout(function () {
   modal.style.display = "inline";
 }, 1500);
 
-closeModal.addEventListener("click", function () {
+modalCloseBtn.addEventListener("click", () => {
   modal.style.display = "none";
+});
+
+consentForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const consentFormData = new FormData(consentForm);
+  const fullName = consentFormData.get("fullName");
+
+  console.log(consentFormData);
+  modalText.innerHTML = `<div class="modal-inner-loading">
+  <img src="images/loading.svg" class="loading">
+  <p id="uploadText">
+      Uploading your data to the dark web...
+  </p>
+</div>`;
+  setTimeout(() => {
+    document.getElementById("uploadText").innerText = `Make the sale...`;
+  }, 1500);
+
+  setTimeout(() => {
+    modalInner.innerHTML = `<h2>Thanks <span class="modal-display-name">${fullName}</span>, you sucker! </h2>
+    <p>We just sold the rights to your eternal soul.</p>
+    <div class="idiot-gif">
+        <img src="images/pirate.gif">
+    </div>`;
+    modalCloseBtn.disabled = false;
+  }, 3000);
 });
